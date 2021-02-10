9c7bb09627323d3c7bccb7cbb55da4c097ca7d04
Shakibul Islam <shakib@field.buzz>
2021-02-10 11:12:40 +0600

    -- Install script updated

--- install_pghoard.sh
+++ install_pghoard.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Activate project's virtual environment
+git checkout 2.1.0-update
+git pull
 python uninstall pghoard
 python setup.py sdist
 pip3 install dist/pghoard-2.1.0.dev101.tar.gz
